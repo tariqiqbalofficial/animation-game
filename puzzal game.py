1dbbd45a8a19270fa28ec7e7ce2f0e6d08746e8b
tariqiqbalofficial <157413892+tariqiqbalofficial@users.noreply.github.com>
2024-02-28 09:51:04 +0500

    Update puzzal game.py

--- puzzal game.py
+++ puzzal game.py
@@ -1,7 +1,8 @@
-import streamlit as
+import streamlit as st
 import random
 
 st.title("Bano Qabil 2.0")
+
 # List of scrambled sentences
 sentences = [
     "The quick brown fox jumps over the lazy dog.",
@@ -9,13 +10,7 @@ sentences = [
     "Streamlit is an open-source app framework for Machine Learning and Data Science.",
     "Artificial Intelligence is shaping the future of technology.",
     "Data visualization is an important skill for data scientists."
-    def main():
-    st.image("https://images.app.goo.gl/s7FbDervZkpWYfSM7", use_column_width=True)
 ]
-
-st.sidebar.title("Navigation")
-    tab = st.sidebar.radio("", ["Home", "About us", "Contact us"])
-
 
 # Function to scramble the words in a sentence
 def scramble_sentence(sentence):
@@ -28,6 +23,18 @@ def check_answer(original_sentence, user_answer):
     return original_sentence == user_answer
 
 def main():
+    st.sidebar.title("Navigation")
+    tab = st.sidebar.radio("", ["Home", "About us", "Contact us"])
+
+    if tab == "Home":
+        st.image("https://images.app.goo.gl/s7FbDervZkpWYfSM7", use_column_width=True)
+    elif tab == "About us":
+        st.title("About Us")
+        st.markdown("Team Name: Pythonic Innovators\n\nTeam Members:\n\nTeam Leader: Syed Muhammad Shujaat Ali\nMember: Abdul Rahman\nMember: Anus Khan\n\nProject Description:\nThe final project submit in Bano Qabil 2.0\nBy Python coding robust puzzle game using Python.")
+    elif tab == "Contact us":
+        st.title("Contact Us")
+        st.write("Email: shujju.sjt6969@gmail.com")       
+
     st.title("Puzzle Game")
 
     # Select a random sentence from the list
@@ -44,12 +51,6 @@ def main():
             st.success("Congratulations! Your answer is correct.")
         else:
             st.error("Sorry, your answer is incorrect. Please try again.")
-    elif tab == "About us":
-        st.title("About Us")
-        st.markdown("Team Name: Pythonic Innovators\n\nTeam Members:\n\nTeam Leader: Syed Muhammad Shujaat Ali\nMember: Abdul Rahman\nMember: Anus Khan\n\nProject Description:\nThe final project submit in Bano Qabil 2.0\nBy Python coding robust puzzle game using Python.")
-    elif tab == "Contact us":
-        st.title("Contact Us")
-        st.write("Email: shujju.sjt6969@gmail.com")       
 
-if name == "main":
+if __name__ == "__main__":
     main()
